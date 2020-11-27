8a48a7ed2049c73483c430b628a0b45b0a89a5b4
André Nogueira <andre.nogueira@epfl.ch>
2020-11-27 15:28:10 +0100

    initial commit

---
+++ index.ts
@@ -0,0 +1,31 @@
+import fastify from 'fastify'
+
+import tsplugin from 'tsplugin'
+
+import { TypeABC } from './TypeABC'
+
+declare module 'fastify' {
+  interface FastifyInstance {
+    decoratorA: TypeABC
+  }
+}
+
+const server = fastify()
+
+server.register(tsplugin, { optionA: 'optionA value' }) // all good here
+
+const decoratorA = { propertyA: 'some value' } as TypeABC
+server.decorate('decoratorA', decoratorA)
+
+server.register(async (instance) => {
+  const { decoratorA } = instance // all good here
+  // ...
+})
+
+server.listen(8080, (err, address) => {
+  if (err) {
+    console.error(err)
+    process.exit(1)
+  }
+  console.log(`Server listening at ${address}`)
+})
